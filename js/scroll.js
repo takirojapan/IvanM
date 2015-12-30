3ad2c039560d229f6d2ad9885874163a685149e4
Takiro <takirojapan@gmail.com>
2015-12-30 13:32:54 -0600

    Pagina Web

---
+++ js/scroll.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+	$('a').click(function(e){
+		e.preventDefault();
+		$('html, body').stop().animate({scrollTop: $($(this).attr('href')).offset().top}, 1000);
+	});
+});
+
+
+
